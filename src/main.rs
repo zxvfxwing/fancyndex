c6b152a3f3c6e9cf6d7f0ae8c4ef945f34ea17cd
spokendotcpp <zxvfxwing@protonmail.com>
2017-11-14 11:51:33 +0100

    Override FromSegments traits on PathBuf to accept dot in routes

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ use rocket_contrib::Json;
 use rocket::response::Redirect;
 use rocket::response::NamedFile;
 use rocket::State;
+use rocket::http::RawStr;
 
 /* Walkdir */
 /*use walkdir::{DirEntry, WalkDir}; */
@@ -32,6 +33,81 @@ use filesystem::directory::Directory;
 use filesystem::walkdir::WalkDir;
 use conf::Config;
 
+
+
+
+
+use rocket::http::uri::{URI, Segments, SegmentError};
+use rocket::request::FromSegments;
+use std::fmt::Debug;
+
+struct UnsafePath {
+    path: PathBuf
+}
+
+impl UnsafePath {
+    pub fn new() -> UnsafePath {
+        UnsafePath {
+            path: PathBuf::new()
+        }
+    }
+
+    pub fn push_to_path(&mut self, suffix: &str) {
+        self.path.push(suffix);
+    }
+
+    pub fn pop_path(&mut self) {
+        self.path.pop();
+    }
+
+    pub fn get_path(&self) -> PathBuf {
+        self.path.to_path_buf()
+    }
+}
+
+impl<'a> FromSegments<'a> for UnsafePath {
+    type Error = SegmentError;
+
+    fn from_segments(segments: Segments<'a>) -> Result<UnsafePath, SegmentError> {
+        let mut unsafe_p = UnsafePath::new();
+
+        //let mut buf = PathBuf::new();
+        for segment in segments {
+            let decoded = URI::percent_decode(segment.as_bytes())
+                .map_err(|e| SegmentError::Utf8(e))?;
+
+            if decoded == ".." {
+
+                unsafe_p.pop_path();
+                //buf.pop();
+            } /*else if decoded.starts_with('.') {
+                return Err(SegmentError::BadStart('.'))
+            } */else if decoded.starts_with('*') {
+                return Err(SegmentError::BadStart('*'))
+            } else if decoded.ends_with(':') {
+                return Err(SegmentError::BadEnd(':'))
+            } else if decoded.ends_with('>') {
+                return Err(SegmentError::BadEnd('>'))
+            } else if decoded.ends_with('<') {
+                return Err(SegmentError::BadEnd('<'))
+            } else if decoded.contains('/') {
+                return Err(SegmentError::BadChar('/'))
+            } else if cfg!(windows) && decoded.contains('\\') {
+                return Err(SegmentError::BadChar('\\'))
+            } else {
+                unsafe_p.push_to_path(&*decoded)
+                //buf.push(&*decoded)
+            }
+        }
+
+        Ok(unsafe_p)
+        //Ok(buf)
+    }
+}
+
+
+
+
 #[get("/api")]
 fn home(cfg: State<Config>) -> Json<Directory> {
     let path = filesystem::get_parent_cdir();
@@ -44,14 +120,19 @@ fn home(cfg: State<Config>) -> Json<Directory> {
 }
 
 #[get("/api/<path..>")]
-fn path(path: PathBuf, cfg: State<Config>) -> Json<Directory> {
-    let path = filesystem::get_parent_cdir().join(path);
+fn path(path: UnsafePath, cfg: State<Config>) -> Json<Directory> {
+    let path = filesystem::get_parent_cdir().join(path.get_path());
     let walker = WalkDir::init(&path)
         .do_hidden(cfg.walk_options.do_hidden)
         .do_symlink(cfg.walk_options.do_symlink)
         .binary_unit(cfg.unit_options.binary_unit);
 
     Json(walker.run())
+}
+
+#[get("/hello/<name..>")]
+fn hello(name: PathBuf) -> String {
+    format!("Hello, {}!", name.display())
 }
 
 #[get("/home")]
@@ -84,7 +165,7 @@ fn main() {
 
     rocket::ignite()
         .manage(cfg)
-        .mount("/", routes![home, path, homee, pathe])
+        .mount("/", routes![home, path, homee, pathe, hello])
         .attach(Template::fairing())
         .launch();
 }
