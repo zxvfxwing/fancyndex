c79a276b43119ffb1be0f042716d1902beebf13b
spokendotcpp <zxvfxwing@protonmail.com>
2017-11-24 13:19:38 +0100

    fix reverse and sort client side functions

--- web/js/index.js
+++ web/js/index.js
@@ -2,9 +2,11 @@ const float_to_fixed = 2;
 
 /* Number of cells */
 const cell_name = 1;
-const cell_date = 2;
-const cell_size = 3;
-const cell_unit = 4;
+const cell_datetime = 2;
+const cell_timestamp = 3;
+const cell_size = 4;
+const cell_byte_size = 5;
+const cell_unit = 6;
 
 const home = "/home";
 
@@ -47,21 +49,34 @@ function decode_utf8(s) {
 
 function th_click(th_class) {
     if( th_class != _by_ ) {
-        window.location.href = pathname + "?by=" + th_class + "&ascending=true";
-    }
-    else {
+        _by_ = th_class;
 
-        if( _ascending_ == "true" ) {
-            document.getElementById("chevron").src = "/asset/open-iconic-master/svg/chevron-top.svg"
-            _ascending_ = "false";
+        if( _by_ == "time" ){
+            insertion_sort(get_directories(), sort_by_time);
+            insertion_sort(get_files(), sort_by_time);
+        }
+        else
+        if( _by_ == "size" ){
+            insertion_sort(get_directories(), sort_by_size);
+            insertion_sort(get_files(), sort_by_size);
         }
         else {
-            document.getElementById("chevron").src = "/asset/open-iconic-master/svg/chevron-bottom.svg"
-            _ascending_ = "true";
+            insertion_sort(get_directories(), sort_by_name);
+            insertion_sort(get_files(), sort_by_name);
+            _by_ = "name";
         }
+    }
+    else {
+        reverse_order();
+    }
 
-        reverse_order("dir_", nbDir);
-        reverse_order("file_", nbFile);
+    if( _ascending_ == "true" ) {
+        document.getElementById("chevron").src = "/asset/open-iconic-master/svg/chevron-top.svg"
+        _ascending_ = "false";
+    }
+    else {
+        document.getElementById("chevron").src = "/asset/open-iconic-master/svg/chevron-bottom.svg"
+        _ascending_ = "true";
     }
 }
 
@@ -105,14 +120,13 @@ function update_dirs_size(DirJSON) {
 
     for(var i=0; i < DirJSON.directories.length; ++i) {
         var dir = DirJSON.directories[i];
-
         var dir_tr = document.getElementById("dir_"+i);
 
         /* If user wants it to be sorted by size, we have to change also name / datetime place */
         if( _by_ == "size" ) {
-
             dir_tr.cells[cell_name].innerHTML = dir.name;
-            dir_tr.cells[cell_date].innerHTML = dir.datetime;
+            dir_tr.cells[cell_datetime].innerHTML = dir.datetime;
+            dir_tr.cells[cell_timestamp].innerHTML = dir.timestamp;
         }
 
         if( String(dir.hsize).includes(".") ) {
@@ -122,6 +136,7 @@ function update_dirs_size(DirJSON) {
             dir_tr.cells[cell_size].innerHTML = dir.hsize;
         }
 
+        dir_tr.cells[cell_byte_size].innerHTML = dir.size;
         dir_tr.cells[cell_unit].innerHTML = dir.short_unit;
     }
 }
@@ -172,23 +187,61 @@ if( nbDir > 0 ) {
 
 //reverse_order("dir_", nbDir);
 
-function reverse_order(id_prefix, nb_elements) {
-    var start = 0;
-    var end = nb_elements - 1;
+function sort_by_size(el_one, el_two) {
+    return el_one.cells[cell_byte_size].innerHTML > el_two.cells[cell_byte_size].innerHTML;
+}
 
-    while( (end - start) > 0 ) {
-        swap_elements(id_prefix, start, end);
-        ++start;
-        --end;
+function sort_by_time(el_one, el_two) {
+    return el_one.cells[cell_timestamp].innerHTML > el_two.cells[cell_timestamp].innerHTML;
+}
+
+function sort_by_name(el_one, el_two) {
+    return el_one.cells[cell_name].innerHTML.toLowerCase() > el_two.cells[cell_name].innerHTML.toLowerCase();
+}
+
+function insertion_sort(arr, sort_func) {
+    var i, j, tmp;
+
+    for(i=0; i < arr.length; ++i) {
+        for(j=i; j > 0; --j) {
+            if( sort_func(arr[j-1], arr[j]) ) {
+                swap_elements(arr[j-1], arr[j]);
+            }
+            else break;
+        }
     }
 }
 
-function swap_elements(id_prefix, one, two) {
-    var el_one = document.getElementById(id_prefix+one);
-    var el_two = document.getElementById(id_prefix+two);
+function get_directories() {
+    return document.getElementsByClassName("is-directory");
+}
 
+function get_files() {
+    return document.getElementsByClassName("is-file");
+}
+
+function reverse_order() {
+    var dir_arr = get_directories();
+    var file_arr = get_files();
+
+    var start = 0;
+    var end = dir_arr.length-1;
+    while( (end - start) > 0 ) {
+        swap_elements( dir_arr[start++], dir_arr[end--] );
+    }
+
+    start = 0;
+    end = file_arr.length-1;
+
+    while( (end - start) > 0 ) {
+        swap_elements( file_arr[start++], file_arr[end--] );
+    }
+}
+
+function swap_elements(el_one, el_two) {
     var tmp_tr = el_one.innerHTML;
     var tmp_id = el_one.id;
+
     el_one.innerHTML = el_two.innerHTML;
     el_one.id = el_two.id;
 
