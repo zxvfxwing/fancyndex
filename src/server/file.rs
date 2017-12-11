debec7a7e8b336843d9327d0c14d9896c647726c
spokendotcpp <zxvfxwing@protonmail.com>
2017-12-11 23:28:50 +0100

    Again, some try on static files serve

--- src/server/file.rs
+++ src/server/file.rs
@@ -1,18 +1,12 @@
 use std::path::{Path, PathBuf};
-use rocket::State;
+use std::io;
 use rocket::response::NamedFile;
-
-use rocket::response::{content, Stream};
 
 use filesystem::{self, get_parent_cdir};
 use filesystem::unsafepath::UnsafePBuf;
 
-use std::io::{self, repeat, Repeat, Read, Take};
-use std::fs::File;
-
 #[get("/<pfile..>")]
-fn big_file(pfile: UnsafePBuf) -> io::Result<Stream<File>>  {
+fn big_file(pfile: UnsafePBuf) -> io::Result<NamedFile> {
     let absolute_path = Path::new(&filesystem::get_parent_cdir()).join(pfile.path());
-    File::open( filesystem::get_path_string(&absolute_path) ).map(|file| Stream::from(file))
-    /*NamedFile::open(Path::new(&filesystem::get_parent_cdir()).join(file.path())).ok()*/
+    NamedFile::open(absolute_path)
 }
