e641c7395af8f1e3ed0b3ec99c356fa908d901f0
zxvfxwing <zxvfxwing@protonmail.com>
2018-01-23 01:25:04 +0100

    Add default behavior if fileconf parsing fails

--- src/config.rs
+++ src/config.rs
@@ -10,10 +10,17 @@ pub struct Folder {
     pub path: String,
 }
 
+#[derive(Deserialize)]
+pub struct WalkOpt {
+    pub hidden: bool,
+    pub symlink: bool,
+}
+
 /// Config abstract object. Respresents fileconf itself.
 #[derive(Deserialize)]
 pub struct Config {
     pub folder: Folder,
+    pub walk_opt: WalkOpt,
 }
 
 impl Config {
@@ -28,10 +35,28 @@ impl Config {
                 /// Inner test to watch if toml parsing is OK
                 match toml::from_str(&data) {
                     Ok(cfg) => return cfg,
-                    Err(e) => panic!("Error while parsing TOML file {:} !\n{:?}", filename, e),
+                    Err(e) => {
+                        println!("Error while parsing TOML file {:} !\n{:?}", filename, e);
+                        println!("Fancyndex will now use a default configuration.");
+                        return Config::default()
+                    }
                 }
             },
             Err(e) => panic!("Error while reading {:} !\n{:?}", filename, e),
+        }
+    }
+
+    /// Returns a default Config object.
+    /// Triggered when TOML parsing fails.
+    pub fn default() -> Config {
+        return Config {
+            folder: Folder {
+                path: "..".to_string(),
+            },
+            walk_opt: WalkOpt {
+                hidden: false,
+                symlink: false,
+            },
         }
     }
 
@@ -48,6 +73,6 @@ impl Config {
             self.folder.path = "..".to_string();
         }
 
-        return self;
+        return self
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,14 @@ use config::Config;
 fn main() {
     let cfg = Config::new("Fancyndex.toml").check();
     println!("{:}", cfg.folder.path);
+    println!("{:}", cfg.walk_opt.hidden);
+    println!("{:}", cfg.walk_opt.symlink);
 
-    for entry in WalkDir::new(cfg.folder.path) {
+    
+
+    /*for entry in WalkDir::new(cfg.folder.path) {
         let entry = entry.unwrap();
         println!("{}", entry.path().display());
     }
+    */
 }
